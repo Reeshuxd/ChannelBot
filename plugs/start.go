85c465fa682e997f02aa8543d3b98fe8d0aa3f0b
ReeshuXD <87052176+Reeshuxd@users.noreply.github.com>
2021-12-10 18:44:33 +0530

    Add files via upload

--- plugs/start.go
+++ plugs/start.go
@@ -1,1 +1,38 @@
+package plugs
 
+import (
+    "github.com/PaulSonOfLars/gotgbot/v2"
+    "github.com/PaulSonOfLars/gotgbot/v2/ext"
+)
+
+func Start(bot *gotgbot.Bot, ctx *ext.Context) error {
+	var MSG string = `
+*Hello, I am a channel spam
+detector bot*.
+I can ban the channels which
+spams your chat!
+
+*(c) @AboutReeshu*
+	`
+	if ctx.EffectiveChat.Type != "private" {
+		ctx.EffectiveMessage.Reply(
+			bot,
+			"Bot is Alive (:",
+		        &gotgbot.SendMessageOpts{ParseMode: "markdown"},
+		)
+	} else {
+		ctx.EffectiveMessage.Reply(
+			bot,
+			MSG,
+			&gotgbot.SendMessageOpts{
+				ParseMode: "markdown",
+				ReplyMarkup: gotgbot.InlineKeyboardMarkup{
+					InlineKeyboard: [][]gotgbot.InlineKeyboardButton{{
+						{Text: "My Source Code", Url: "github.com/Reeshuxd/ChannelBot"},
+					}},
+				},
+			},
+		)
+	}
+	return nil
+}
